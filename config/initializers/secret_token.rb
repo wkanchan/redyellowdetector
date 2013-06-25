65a31252744f56db76ca463cb3c3c48204fdc508
Knott <wittawat@cmu.edu>
2013-06-24 17:54:23 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redyellowdetector::Application.config.secret_token = '07c629cd7e222597d1f19b2dabbd36ec2b1a1418e50e66420b18236bed4df346912194cf1f7641862ac47b34cdcbf464eaa378abde35d1ce2cb52aace6ea1654'
